3eef1884b405123a9e15221cfc8a2e7906301cd7
Noel <odonnell.noel@gmail.com>
2017-03-26 12:06:26 +0100

    temporary hard-code http request

--- http.c
+++ http.c
@@ -2,9 +2,12 @@
 
 void http_parse_request(const char* buf, http_request* request) {
 
+	// temporary - hard-coding a standard "GET / HTTP/1.1"
+	// TODO - implement
 	request->version = HTTP_VERSION_11;
 	request->verb = HTTP_VERB_GET;
 	request->path[0] = '/';
 	request->path[1] = '\0';
+	request->content_length = 0;
 }
 
